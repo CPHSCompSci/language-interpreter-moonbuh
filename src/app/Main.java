05d3485415e1fac86cc4bbaae67606db4580333a
moonbuh <moonbuh@014-1617-S29FM.students.cisd.conroeisd.loc>
2019-10-08 12:47:16 -0500

    Finished Code

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,5 @@
 package app;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,32 +16,89 @@ public class Main {
    * 
    * @param word The single word to be decoded.
    */
-  public static String decode(String word) {
-    return "";
+  public static String decode(String word)
+  {
+    int I1 = word.indexOf("ent");
+    if(I1!=-1)
+    {
+      char W = word.charAt(I1-1);
+      if(W=='a'||W=='e'||W=='i'||W=='o'||W=='u')
+      {
+        word = word.substring(0,I1)+word.substring(I1+3);
+      }
+    }
+    I1 = word.indexOf("ingy");
+    if(I1!=-1)
+    {
+      if((word.indexOf('.')==word.length()-1||word.indexOf(',')==word.length()-1)&&I1==word.length()-5)
+      {
+        word = word.substring(0,word.length()-5)+word.charAt(word.length()-1);
+      }else if(I1 == word.length()-4)
+      {
+        word = word.substring(0,word.length()-4);
+      }
+    }
+    return word;
   }
+
 
   /**
    * This method should not have code copy/pasted from the decode method.
    * 
    * @param message The sentence (multiple words) to be decoded.
    */
-  public static String decodeMessage(String message) {
-    return "";
+  public static String decodeMessage(String sen)
+  {
+    String NS,W;
+    W = sen;
+    NS = "";
+    boolean TF = true;
+      if(W.indexOf(' ')!=-1)
+      {
+       NS = decode(W.substring(0,W.indexOf(' ')));
+       W = W.substring(W.indexOf(' ')+1);
+      }
+    while(TF)
+    {
+      if(W.indexOf(' ')!=-1)
+      {
+      NS = NS+" "+decode(W.substring(0,W.indexOf(' ')));
+      W = W.substring(W.indexOf(' ')+1);
+      }else
+      {
+        NS = NS+" "+decode(W);
+        TF = false;
+      }
+    }
+    return NS;
   }
 
   /**
    * This method uses the decode(String) and a Scanner to decode a word specified
    * by the user.
    */
-  public static String decodeInput() {
-    return "";
+  public static String decodeInput()
+  {
+    Scanner S2 = new Scanner(System.in);
+    System.out.println("Please Enter The Word or Sentence");
+    String IW; IW = S2.nextLine();
+    if(IW.indexOf(' ')==-1)
+    {
+    return decode(IW);
+    }
+    return decodeMessage(IW);
   }
   
   /**
    * This method uses the Lexicon translate(String) and a Scanner to encode a word specified
    * by the user.
    */
-  public static String encodeInput() {
-    return "";
+  public static String encode(String IW)
+  {
+    Scanner S3 = new Scanner(System.in);
+    Lexicon lex = new Lexicon();
+    System.out.println("Please Enter The WORD");
+
+    return lex.translate(S3.nextLine());
   }
 }
